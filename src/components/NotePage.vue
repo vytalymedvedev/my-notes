39ce84a84101afa8c4818a55a1f8c13831e1c1d9
Vitaly Medvedev <vytalymedvedev@gmail.com>
2023-12-02 20:09:12 +0600

    Add add note button

--- src/components/NotePage.vue
+++ src/components/NotePage.vue
@@ -1,20 +1,30 @@
 <template>
-  <main class="notePage">
-    <NotePrimary
-      v-for="(note, index) of notes"
-      :title="note.title"
-      :content="note.content"
-      :key="index"
-    />
+  <main class="note-page">
+    <div class="note-page__table">
+      <NotePrimary
+        v-for="(note, index) of notes"
+        :title="note.title"
+        :content="note.content"
+        :key="index"
+      />
+    </div>
+    
+    <button-primary @click="handleAddNote" class="note-page__add-note-button">
+      <template slot="icon">
+        <img src="/plus.svg" alt="remove icon" class="note-page__add-note-button-icon">
+      </template>
+    </button-primary>
   </main>
 </template>
 
 <script>
+import ButtonPrimary from './ButtonPrimary.vue';
 import NotePrimary from './NotePrimary.vue';
 
 export default {
   name: 'NotePage',
   components: {
+    ButtonPrimary,
     NotePrimary,
   },
   data() {
@@ -66,31 +76,64 @@ export default {
         },
       ],
     }
+  },
+  methods: {
+    handleAddNote() {
+      
+    }
   }
 }
 </script>
 
 <style scoped>
-.notePage {
+.note-page {
   height: 100vh;
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 20px;
   padding-top: 20px;
 }
 
+.note-page__table {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 20px;
+}
+
 @media screen and (min-width: 768px) {
-	.notePage {
+	.note-page {
 		padding-top: 40px;
 	}
 }
 
-@media screen and (min-width: 1100px) {
-	.notePage {
+@media screen and (min-width: 1920px) {
+	.note-page__table {
 		grid-template-columns: 1fr 1fr 1fr;
     gap: 40px;
     grid-auto-rows: min-content;
 	}
+}
+
+.note-page__add-note-button {
+  position: -webkit-sticky;
+  position: sticky;
+  bottom: 0;
+  box-shadow: 0 15px 46px -10px rgba(0, 0, 0, 0.6);
+  margin-left: auto;
+  margin-right: -1rem;
+}
+
+@media screen and (min-width: 1366px) {
+	.note-page__add-note-button {
+		margin-right: -4rem;
+	}
+}
+
+@media screen and (min-width: 1920px) {
+	.note-page__add-note-button {
+		margin-right: -6rem;
+	}
+}
+
+.note-page__add-note-button-icon {
+  width: 16px;
 }
 </style>
   
